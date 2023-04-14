da3b2bcefccd815b83215cf7ca2e73161a58d292
miag_bio <mia@timg.org>
2023-04-14 11:46:04 -0700

    adding defaults, editing name and return

--- HW1/energyproduction_function.R
+++ HW1/energyproduction_function.R
@@ -1,20 +1,16 @@
 
 library(roxygen2)
 
-#' @name energy
+#' @name pv_energy
 #' @description
 #' This is a basic function to compute energy, in kWh, produced from a photovoltaic system. 
 #' @param A solar panel areas in meters squared
 #' @param r panel yield (0-1), AKA manufacture efficiency (Default: 0.2)
 #' @param PR performance ratio (0-1) (Default: 0.75)
 #' @param H annual average solar radiation (kWh)
-#' @returns A numeric.
+#' @returns E, a numeric value for energy, in kWh.
 
-energy <- function(A, r, H, PR){
-  
-  if(missing(r)) r = 0.2
-  
-  if(missing(PR)) PR = 0.75
+pv_energy <- function(A, r = 0.2, H, PR = 0.75){
   
   E = A * r * H * PR
   
