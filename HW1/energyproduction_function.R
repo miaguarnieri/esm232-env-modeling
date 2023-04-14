a9182441655c72e374ffdd647d350cc047b4acb4
miag_bio <mia@timg.org>
2023-04-14 11:55:44 -0700

    updates

--- HW1/energyproduction_function.R
+++ HW1/energyproduction_function.R
@@ -4,6 +4,7 @@ library(roxygen2)
 #' @name pv_energy
 #' @description
 #' This is a basic function to compute energy, in kWh, produced from a photovoltaic system. 
+#'  
 #' @param A solar panel areas in meters squared
 #' @param r panel yield (0-1), AKA manufacture efficiency (Default: 0.2)
 #' @param PR performance ratio (0-1) (Default: 0.75)
