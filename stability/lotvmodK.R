ad81d3c4b74ae0424df16ce9df9c1dec204877a5
miag_bio <mia@timg.org>
2023-06-09 16:15:32 -0700

    Finalizing the assignment and writeup

--- stability/lotvmodK.R
+++ stability/lotvmodK.R
@@ -7,15 +7,7 @@
 #'  \emph{rprey} is growth rate of prey population;
 #'  \emph{eff} is the rate of ingestion of prey by predators
 #'  \emph{alpha} is a interaction coefficient (higher values greater interaction
-#’  \emph{pmort}  mortality rate of predictor population
-#' @examples
-#' lotvod(t=1, pop=list(1,2), pop=list(0.5,0.3,0.2,0.2))
-#'
-#' pars = c(rprey=0.5, alpha=0.3, eff=0.2, pmort=0.2)
-#' currpop  = c(prey = 1, pred=1)
-#  days = seq(from=1,to=20)
-#' res = ode(func=lotvmod, y=currpop, times=days, parms=pars)
-#'
+#'  \emph{pmort}  mortality rate of predictor population
 #' @return  lotvmod returns a list containing the following components
 #' \describe{
 #' \item{dprey}{rate of change of prey populutation}
